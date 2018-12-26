e76a4aae433e76d7fc5b86fb80cc4c990b1979db
robertodecampos <roberto.rfc@live.com>
2018-12-26 21:29:56 -0200

    Adição do formulário de aceitação do termo de responsabilidade

---
+++ frmTermoResponsabilidade.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ClienteTatoo
+{
+    partial class FormTermoResponsabilidade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbTermoResponsabilidade = new System.Windows.Forms.RichTextBox();
+            this.cbxConcordo = new System.Windows.Forms.CheckBox();
+            this.btnAvancar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbTermoResponsabilidade
+            // 
+            this.rtbTermoResponsabilidade.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbTermoResponsabilidade.Location = new System.Drawing.Point(12, 12);
+            this.rtbTermoResponsabilidade.Name = "rtbTermoResponsabilidade";
+            this.rtbTermoResponsabilidade.ReadOnly = true;
+            this.rtbTermoResponsabilidade.Size = new System.Drawing.Size(776, 392);
+            this.rtbTermoResponsabilidade.TabIndex = 0;
+            this.rtbTermoResponsabilidade.Text = "";
+            // 
+            // cbxConcordo
+            // 
+            this.cbxConcordo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbxConcordo.AutoSize = true;
+            this.cbxConcordo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxConcordo.Location = new System.Drawing.Point(12, 413);
+            this.cbxConcordo.Name = "cbxConcordo";
+            this.cbxConcordo.Size = new System.Drawing.Size(407, 24);
+            this.cbxConcordo.TabIndex = 1;
+            this.cbxConcordo.Text = "Li e concordo com o termo de responsabilidade acima";
+            this.cbxConcordo.UseVisualStyleBackColor = true;
+            this.cbxConcordo.CheckedChanged += new System.EventHandler(this.cbxConcordo_CheckedChanged);
+            // 
+            // btnAvancar
+            // 
+            this.btnAvancar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAvancar.Enabled = false;
+            this.btnAvancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvancar.Location = new System.Drawing.Point(713, 410);
+            this.btnAvancar.Name = "btnAvancar";
+            this.btnAvancar.Size = new System.Drawing.Size(75, 28);
+            this.btnAvancar.TabIndex = 2;
+            this.btnAvancar.Text = "Avançar";
+            this.btnAvancar.UseVisualStyleBackColor = true;
+            // 
+            // FormTermoResponsabilidade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAvancar);
+            this.Controls.Add(this.cbxConcordo);
+            this.Controls.Add(this.rtbTermoResponsabilidade);
+            this.Name = "FormTermoResponsabilidade";
+            this.Text = "Termo de Responsabilidade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTermoResponsabilidade;
+        private System.Windows.Forms.CheckBox cbxConcordo;
+        private System.Windows.Forms.Button btnAvancar;
+    }
+}
---
+++ frmTermoResponsabilidade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClienteTatoo.Model;
+using ClienteTatoo.Utils;
+
+namespace ClienteTatoo
+{
+    public partial class FormTermoResponsabilidade : Form
+    {
+        private TermoResponsabilidade termoResponsabilidade = new TermoResponsabilidade();
+
+        public FormTermoResponsabilidade()
+        {
+            InitializeComponent();
+
+            using (var conn = new Connection())
+            {
+                termoResponsabilidade.SetCurrent(conn, null);
+                rtbTermoResponsabilidade.Text = termoResponsabilidade.Termo;
+            }
+        }
+
+        private void cbxConcordo_CheckedChanged(object sender, EventArgs e) => btnAvancar.Enabled = cbxConcordo.Checked;
+    }
+}
