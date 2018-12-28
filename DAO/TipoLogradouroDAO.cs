9ac14b9b5807dc0baae9326b82ef9eee360b9c19
robertodecampos <roberto.rfc@live.com>
2018-12-27 22:01:20 -0200

    Adicionando classes para controle do tipo de logradouro

---
+++ DAO/TipoLogradouroDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClienteTatoo.Model;
+using ClienteTatoo.Utils;
+using MySql.Data.MySqlClient;
+
+namespace ClienteTatoo.DAO
+{
+    class TipoLogradouroDAO : IDao<TipoLogradouro, MySqlTransaction>
+    {
+        private Connection Conexao { get; set; }
+
+        public TipoLogradouroDAO(Connection conexao) => Conexao = conexao;
+
+        public int Insert(TipoLogradouro model, MySqlTransaction transaction) => throw new NotImplementedException();
+
+        public int Remove(TipoLogradouro model, MySqlTransaction transaction) => throw new NotImplementedException();
+
+        public int Update(TipoLogradouro model, MySqlTransaction transaction) => throw new NotImplementedException();
+
+        public IList<TipoLogradouro> GetAll(MySqlTransaction transaction)
+        {
+            string sql = "SELECT  a.`tipologradouro`" +
+                         " FROM `endereco`.`log_tipo_logr` a";
+
+            DataTable dt = Conexao.ExecuteReader(sql, null, transaction);
+
+            var tiposLogradouro = new List<TipoLogradouro>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                var tipoLogradouro = new TipoLogradouro();
+
+                DistributeData(tipoLogradouro, row);
+
+                tiposLogradouro.Add(tipoLogradouro);
+            }
+
+            return tiposLogradouro;
+        }
+
+        private void DistributeData(TipoLogradouro model, DataRow dr)
+        {
+            model.Nome = dr["tipologradouro"].ToString();
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ Model/TipoLogradouro.cs
@@ -0,0 +1,26 @@
+﻿using ClienteTatoo.DAO;
+using ClienteTatoo.Utils;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteTatoo.Model
+{
+    class TipoLogradouro : IDisposable
+    {
+        public string Nome { get; set; }
+
+        public static IList<TipoLogradouro> GetAll(Connection connection, MySqlTransaction transaction)
+        {
+            using (var dao = new TipoLogradouroDAO(connection))
+            {
+                return dao.GetAll(transaction);
+            }
+        }
+
+        public void Dispose() { }
+    }
+}
