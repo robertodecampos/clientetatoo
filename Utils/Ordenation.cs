3b424699898e7c66890e59c9f54e7ceb18395edf
robertodecampos <roberto.rfc@live.com>
2018-12-30 08:42:49 -0200

    Criando classe genérica para ordenação

---
+++ Utils/Ordenation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteTatoo.Utils
+{
+    public enum TypeOrder {toAsc, toDesc}
+
+    public class Ordenation<FieldOrder>
+    {
+        public FieldOrder Field { get; set; }
+        public TypeOrder Type { get; set; }
+
+        public Ordenation(FieldOrder field, TypeOrder type)
+        {
+            Field = field;
+            Type = type;
+        }
+    }
+}
