bd1ee2f8e5aec7545051650655efb8560817ea42
robertodecampos <roberto.rfc@live.com>
2018-12-30 08:42:19 -0200

    Criando classe genérica para filtros

---
+++ Utils/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteTatoo.Utils
+{
+    public class Filter<FieldFilter>
+    {
+        public FieldFilter Field { get; set; }
+        public object Value { get; set; }
+
+        public Filter(FieldFilter field, object value)
+        {
+            Field = field;
+            Value = value;
+        }
+    }
+}
