94bdf04e70b75073187d6ba5b83dd0c31792cf76
robertodecampos <roberto.rfc@live.com>
2019-01-04 23:54:30 -0200

    Adicionando classe para tratar criptografias

---
+++ Utils/Cryptography.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ClienteTatoo.Utils
+{
+    public static class Cryptography
+    {
+        public static string EncondeMD5(string valor)
+        {
+            MD5 md5Hash = MD5.Create();
+            // Converter a String para array de bytes, que é como a biblioteca trabalha.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(valor));
+
+            // Cria-se um StringBuilder para recompôr a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop para formatar cada byte como uma String em hexadecimal
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+
+            return sBuilder.ToString();
+        }
+    }
+}
