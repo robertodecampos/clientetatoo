249280c05ab77e83bebf3ff5739bccb1d8ad7f05
robertodecampos <roberto.rfc@live.com>
2019-02-09 11:25:15 -0200

    Adicionando formulário para gerenciar as perguntas

---
+++ frmPerguntas.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ClienteTatoo
+{
+    partial class FormPerguntas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvPerguntas = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnAtivarDesativar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsvPerguntas
+            // 
+            this.lsvPerguntas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsvPerguntas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lsvPerguntas.FullRowSelect = true;
+            this.lsvPerguntas.Location = new System.Drawing.Point(12, 12);
+            this.lsvPerguntas.Name = "lsvPerguntas";
+            this.lsvPerguntas.Size = new System.Drawing.Size(636, 235);
+            this.lsvPerguntas.TabIndex = 0;
+            this.lsvPerguntas.UseCompatibleStateImageBehavior = false;
+            this.lsvPerguntas.View = System.Windows.Forms.View.Details;
+            this.lsvPerguntas.SelectedIndexChanged += new System.EventHandler(this.lsvPerguntas_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Código";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Descrição";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tipo";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Obrigatória";
+            this.columnHeader4.Width = 63;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Ativa";
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRemover.Location = new System.Drawing.Point(12, 253);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 1;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdicionar.Location = new System.Drawing.Point(573, 253);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAlterar.Location = new System.Drawing.Point(174, 253);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 3;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnAtivarDesativar
+            // 
+            this.btnAtivarDesativar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAtivarDesativar.Location = new System.Drawing.Point(93, 253);
+            this.btnAtivarDesativar.Name = "btnAtivarDesativar";
+            this.btnAtivarDesativar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtivarDesativar.TabIndex = 4;
+            this.btnAtivarDesativar.Text = "(Des)Ativar";
+            this.btnAtivarDesativar.UseVisualStyleBackColor = true;
+            this.btnAtivarDesativar.Click += new System.EventHandler(this.btnAtivarDesativar_Click);
+            // 
+            // FormPerguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 288);
+            this.Controls.Add(this.btnAtivarDesativar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.lsvPerguntas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPerguntas";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Perguntas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvPerguntas;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnAtivarDesativar;
+    }
+}
---
+++ frmPerguntas.cs
@@ -0,0 +1,169 @@
+﻿using ClienteTatoo.Model;
+using ClienteTatoo.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteTatoo
+{
+    public partial class FormPerguntas : Form
+    {
+        private List<Pergunta> Perguntas { get; set; }
+        private TipoPergunta TipoPergunta { get; set; }
+
+        public FormPerguntas(TipoPergunta tipoPergunta)
+        {
+            InitializeComponent();
+
+            TipoPergunta = tipoPergunta;
+
+            switch (tipoPergunta)
+            {
+                case TipoPergunta.Cliente:
+                    Text += " para Clientes";
+                    break;
+                case TipoPergunta.Tatuagem:
+                    Text += " para Tatuagens";
+                    break;
+            }
+
+            OrganizarColunas();
+
+            CarregarPerguntas();
+        }
+
+        private void AtivarDesativarAcoes()
+        {
+            btnRemover.Visible = (lsvPerguntas.SelectedIndices.Count > 0);
+            btnAtivarDesativar.Visible = GetVisibleAtivarDesativar();
+            btnAlterar.Visible = lsvPerguntas.SelectedIndices.Count == 1;
+        }
+
+        private void CarregarPerguntas()
+        {
+            using (var conn = new Connection())
+            {
+                Perguntas = Pergunta.GetPrincipaisByTipoPergunta(TipoPergunta, false, conn, null);
+            }
+
+            lsvPerguntas.Items.Clear();
+
+            foreach (Pergunta pergunta in Perguntas)
+            {
+                var item = new ListViewItem();
+
+                item.Text = pergunta.Id.ToString();
+                item.SubItems.Add(pergunta.Descricao);
+                if (pergunta.RespostaDissertativa)
+                    item.SubItems.Add("Dissertativa");
+                else if (pergunta.RespostaUnica)
+                    item.SubItems.Add("Seleção Única");
+                else
+                    item.SubItems.Add("Múltipla Seleção");
+                item.SubItems.Add(pergunta.Obrigatoria ? "Sim" : "Não");
+                item.SubItems.Add(pergunta.Ativada ? "Sim" : "Não");
+
+                lsvPerguntas.Items.Add(item);
+            }
+
+            AtivarDesativarAcoes();
+        }
+
+        private void OrganizarColunas()
+        {
+            int width = lsvPerguntas.ClientSize.Width;
+
+            for (int i = 0; i < lsvPerguntas.Columns.Count; i++)
+                width -= lsvPerguntas.Columns[i].Width;
+
+            lsvPerguntas.Columns[1].Width += width;
+        }
+
+        private bool GetVisibleAtivarDesativar()
+        {
+            int qtdeAtivas = 0;
+
+            for (int i = 0; i < lsvPerguntas.SelectedIndices.Count; i++)
+            {
+                if (Perguntas[lsvPerguntas.SelectedIndices[i]].Ativada)
+                    qtdeAtivas += 1;
+            }
+
+            if ((lsvPerguntas.SelectedIndices.Count > 0) && ((qtdeAtivas == lsvPerguntas.SelectedIndices.Count) || (qtdeAtivas == 0)))
+            {
+                btnAtivarDesativar.Text = qtdeAtivas == 0 ? "Ativar" : "Desativar";
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            using (var frmPergunta = new FormPergunta(TipoPergunta))
+            {
+                if (frmPergunta.ShowDialog() == DialogResult.OK)
+                    CarregarPerguntas();
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (lsvPerguntas.SelectedIndices.Count != 1)
+                return;
+
+            using (var frmPergunta = new FormPergunta(Perguntas[lsvPerguntas.SelectedIndices[0]]))
+            {
+                if (frmPergunta.ShowDialog() == DialogResult.OK)
+                    CarregarPerguntas();
+            }
+        }
+
+        private void lsvPerguntas_SelectedIndexChanged(object sender, EventArgs e) => AtivarDesativarAcoes();
+
+        private void btnAtivarDesativar_Click(object sender, EventArgs e)
+        {
+            if (!GetVisibleAtivarDesativar())
+                return;
+
+            bool ativar = !Perguntas[lsvPerguntas.SelectedIndices[0]].Ativada;
+            int qtdeSelecionada = lsvPerguntas.SelectedIndices.Count;
+
+            string mensagemPerguntasSelecionadas = $" {(ativar ? "ativar" : "desativar")}" +
+                $" {(qtdeSelecionada == 1 ? $"a pergunta \"{Perguntas[lsvPerguntas.SelectedIndices[0]].Descricao}\"" : $"as {qtdeSelecionada} perguntas selecionadas")}?\n" +
+                "Essa ação poderá ser revertida posteriormente!";
+
+            if (MessageBox.Show("Deseja realmente" + mensagemPerguntasSelecionadas, "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                return;
+
+            using (var conn = new Connection())
+            using (SQLiteTransaction transaction = conn.BeginTransaction())
+            {
+                try
+                {
+                    for (int i = 0; i < qtdeSelecionada; i++)
+                    {
+                        Perguntas[i].Ativada = ativar;
+                        Perguntas[i].Salvar(conn, transaction);
+                    }
+
+                    transaction.Commit();
+                    MessageBox.Show($"{(qtdeSelecionada == 1 ? $"Pergunta {(ativar ? "ativada" : "desativada")}" : $"Perguntas {(ativar ? "ativadas" : "desativadas")}")} com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CarregarPerguntas();
+                } catch (Exception erro)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Ocorreu um erro ao" + mensagemPerguntasSelecionadas + "\n\n" + erro.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
