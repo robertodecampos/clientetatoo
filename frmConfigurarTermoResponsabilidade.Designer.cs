df49e338bbe17edf1882205f401800bb6a7c65a5
Roberto de Campos <roberto.rfc@live.com>
2020-02-09 10:26:21 -0300

    Adicionando configuração de estilos do termo de responsabilidade

--- frmConfigurarTermoResponsabilidade.Designer.cs
+++ frmConfigurarTermoResponsabilidade.Designer.cs
@@ -28,8 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConfigurarTermoResponsabilidade));
             this.rtbTermo = new System.Windows.Forms.RichTextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
+            this.tsMenu = new System.Windows.Forms.ToolStrip();
+            this.toolStripCopiar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripColar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripNegrito = new System.Windows.Forms.ToolStripButton();
+            this.toolStripItalico = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSublinhado = new System.Windows.Forms.ToolStripButton();
+            this.toolStripRiscado = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripFontFamily = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripTamanho = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripEsquerda = new System.Windows.Forms.ToolStripButton();
+            this.toolStripCentralizado = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDireita = new System.Windows.Forms.ToolStripButton();
+            this.toolStripCorFonte = new System.Windows.Forms.ToolStripButton();
+            this.toolStripCorFundo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.cdSelecionarCor = new System.Windows.Forms.ColorDialog();
+            this.tsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbTermo
@@ -37,11 +58,12 @@
             this.rtbTermo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbTermo.Location = new System.Drawing.Point(12, 12);
+            this.rtbTermo.Location = new System.Drawing.Point(12, 28);
             this.rtbTermo.Name = "rtbTermo";
-            this.rtbTermo.Size = new System.Drawing.Size(777, 393);
+            this.rtbTermo.Size = new System.Drawing.Size(777, 377);
             this.rtbTermo.TabIndex = 0;
             this.rtbTermo.Text = "";
+            this.rtbTermo.SelectionChanged += new System.EventHandler(this.rtbTermo_SelectionChanged);
             // 
             // btnSalvar
             // 
@@ -54,17 +76,204 @@
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
+            // tsMenu
+            // 
+            this.tsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripCopiar,
+            this.toolStripColar,
+            this.toolStripSeparator1,
+            this.toolStripFontFamily,
+            this.toolStripTamanho,
+            this.toolStripSeparator2,
+            this.toolStripNegrito,
+            this.toolStripItalico,
+            this.toolStripSublinhado,
+            this.toolStripRiscado,
+            this.toolStripSeparator3,
+            this.toolStripCorFonte,
+            this.toolStripCorFundo,
+            this.toolStripSeparator4,
+            this.toolStripEsquerda,
+            this.toolStripCentralizado,
+            this.toolStripDireita});
+            this.tsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tsMenu.Name = "tsMenu";
+            this.tsMenu.Size = new System.Drawing.Size(800, 25);
+            this.tsMenu.TabIndex = 1;
+            this.tsMenu.Text = "toolStrip1";
+            // 
+            // toolStripCopiar
+            // 
+            this.toolStripCopiar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripCopiar.Image = ((System.Drawing.Image)(resources.GetObject("toolStripCopiar.Image")));
+            this.toolStripCopiar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripCopiar.Name = "toolStripCopiar";
+            this.toolStripCopiar.Size = new System.Drawing.Size(23, 22);
+            this.toolStripCopiar.Text = "Copiar";
+            this.toolStripCopiar.Click += new System.EventHandler(this.toolStripCopiar_Click);
+            // 
+            // toolStripColar
+            // 
+            this.toolStripColar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripColar.Image = ((System.Drawing.Image)(resources.GetObject("toolStripColar.Image")));
+            this.toolStripColar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripColar.Name = "toolStripColar";
+            this.toolStripColar.Size = new System.Drawing.Size(23, 22);
+            this.toolStripColar.Text = "Colar";
+            this.toolStripColar.Click += new System.EventHandler(this.toolStripColar_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripNegrito
+            // 
+            this.toolStripNegrito.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripNegrito.Image = ((System.Drawing.Image)(resources.GetObject("toolStripNegrito.Image")));
+            this.toolStripNegrito.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripNegrito.Name = "toolStripNegrito";
+            this.toolStripNegrito.Size = new System.Drawing.Size(23, 22);
+            this.toolStripNegrito.Text = "Negrito";
+            this.toolStripNegrito.Click += new System.EventHandler(this.toolStripNegrito_Click);
+            // 
+            // toolStripItalico
+            // 
+            this.toolStripItalico.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripItalico.Image = ((System.Drawing.Image)(resources.GetObject("toolStripItalico.Image")));
+            this.toolStripItalico.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripItalico.Name = "toolStripItalico";
+            this.toolStripItalico.Size = new System.Drawing.Size(23, 22);
+            this.toolStripItalico.Text = "Itálico";
+            this.toolStripItalico.Click += new System.EventHandler(this.toolStripItalico_Click);
+            // 
+            // toolStripSublinhado
+            // 
+            this.toolStripSublinhado.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSublinhado.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSublinhado.Image")));
+            this.toolStripSublinhado.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSublinhado.Name = "toolStripSublinhado";
+            this.toolStripSublinhado.Size = new System.Drawing.Size(23, 22);
+            this.toolStripSublinhado.Text = "Sublinhado";
+            this.toolStripSublinhado.Click += new System.EventHandler(this.toolStripSublinhado_Click);
+            // 
+            // toolStripRiscado
+            // 
+            this.toolStripRiscado.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripRiscado.Image = ((System.Drawing.Image)(resources.GetObject("toolStripRiscado.Image")));
+            this.toolStripRiscado.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripRiscado.Name = "toolStripRiscado";
+            this.toolStripRiscado.Size = new System.Drawing.Size(23, 22);
+            this.toolStripRiscado.Text = "Riscado";
+            this.toolStripRiscado.Click += new System.EventHandler(this.toolStripRiscado_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripFontFamily
+            // 
+            this.toolStripFontFamily.Name = "toolStripFontFamily";
+            this.toolStripFontFamily.Size = new System.Drawing.Size(200, 25);
+            this.toolStripFontFamily.TextChanged += new System.EventHandler(this.toolStripFontFamily_TextChanged);
+            // 
+            // toolStripTamanho
+            // 
+            this.toolStripTamanho.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "18",
+            "24",
+            "30",
+            "36",
+            "48",
+            "60",
+            "72",
+            "96"});
+            this.toolStripTamanho.Name = "toolStripTamanho";
+            this.toolStripTamanho.Size = new System.Drawing.Size(75, 25);
+            this.toolStripTamanho.TextChanged += new System.EventHandler(this.toolStripTamanho_TextChanged);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripEsquerda
+            // 
+            this.toolStripEsquerda.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripEsquerda.Image = ((System.Drawing.Image)(resources.GetObject("toolStripEsquerda.Image")));
+            this.toolStripEsquerda.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripEsquerda.Name = "toolStripEsquerda";
+            this.toolStripEsquerda.Size = new System.Drawing.Size(23, 22);
+            this.toolStripEsquerda.Text = "Esquerda";
+            this.toolStripEsquerda.Click += new System.EventHandler(this.toolStripEsquerda_Click);
+            // 
+            // toolStripCentralizado
+            // 
+            this.toolStripCentralizado.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripCentralizado.Image = ((System.Drawing.Image)(resources.GetObject("toolStripCentralizado.Image")));
+            this.toolStripCentralizado.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripCentralizado.Name = "toolStripCentralizado";
+            this.toolStripCentralizado.Size = new System.Drawing.Size(23, 22);
+            this.toolStripCentralizado.Text = "Centralizado";
+            this.toolStripCentralizado.Click += new System.EventHandler(this.toolStripCentralizado_Click);
+            // 
+            // toolStripDireita
+            // 
+            this.toolStripDireita.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDireita.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDireita.Image")));
+            this.toolStripDireita.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDireita.Name = "toolStripDireita";
+            this.toolStripDireita.Size = new System.Drawing.Size(23, 22);
+            this.toolStripDireita.Text = "Direita";
+            this.toolStripDireita.Click += new System.EventHandler(this.toolStripDireita_Click);
+            // 
+            // toolStripCorFonte
+            // 
+            this.toolStripCorFonte.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripCorFonte.Image = ((System.Drawing.Image)(resources.GetObject("toolStripCorFonte.Image")));
+            this.toolStripCorFonte.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripCorFonte.Name = "toolStripCorFonte";
+            this.toolStripCorFonte.Size = new System.Drawing.Size(23, 22);
+            this.toolStripCorFonte.Text = "Cor da fonte";
+            this.toolStripCorFonte.Click += new System.EventHandler(this.toolStripCorFonte_Click);
+            // 
+            // toolStripCorFundo
+            // 
+            this.toolStripCorFundo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripCorFundo.Image = ((System.Drawing.Image)(resources.GetObject("toolStripCorFundo.Image")));
+            this.toolStripCorFundo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripCorFundo.Name = "toolStripCorFundo";
+            this.toolStripCorFundo.Size = new System.Drawing.Size(23, 22);
+            this.toolStripCorFundo.Text = "Cor do fundo";
+            this.toolStripCorFundo.Click += new System.EventHandler(this.toolStripCorFundo_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
             // FormConfigurarTermoResponsabilidade
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tsMenu);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.rtbTermo);
             this.Name = "FormConfigurarTermoResponsabilidade";
             this.Text = "Configurar Termo de Responsibilidade";
             this.Load += new System.EventHandler(this.FormConfigurarTermoResponsabilidade_Load);
+            this.tsMenu.ResumeLayout(false);
+            this.tsMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +281,24 @@
 
         private System.Windows.Forms.RichTextBox rtbTermo;
         private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ToolStrip tsMenu;
+        private System.Windows.Forms.ToolStripButton toolStripCopiar;
+        private System.Windows.Forms.ToolStripButton toolStripColar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripNegrito;
+        private System.Windows.Forms.ToolStripButton toolStripItalico;
+        private System.Windows.Forms.ToolStripButton toolStripSublinhado;
+        private System.Windows.Forms.ToolStripButton toolStripRiscado;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripComboBox toolStripFontFamily;
+        private System.Windows.Forms.ToolStripComboBox toolStripTamanho;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripCorFonte;
+        private System.Windows.Forms.ToolStripButton toolStripCorFundo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton toolStripEsquerda;
+        private System.Windows.Forms.ToolStripButton toolStripCentralizado;
+        private System.Windows.Forms.ToolStripButton toolStripDireita;
+        private System.Windows.Forms.ColorDialog cdSelecionarCor;
     }
 }
--- frmConfigurarTermoResponsabilidade.cs
+++ frmConfigurarTermoResponsabilidade.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,13 @@ namespace ClienteTatoo
         public FormConfigurarTermoResponsabilidade()
         {
             InitializeComponent();
+
+            FontFamily[] families = new InstalledFontCollection().Families;
+
+            foreach (FontFamily family in families)
+            {
+                toolStripFontFamily.Items.Add(family.Name);
+            }
         }
 
         private void FormConfigurarTermoResponsabilidade_Load(object sender, EventArgs e)
@@ -56,6 +64,103 @@ namespace ClienteTatoo
             {
                 MessageBox.Show("Ocorreu um erro ao salvar o termo: " + erro.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void toolStripCopiar_Click(object sender, EventArgs e)
+        {
+            rtbTermo.Copy();
+        }
+
+        private void toolStripColar_Click(object sender, EventArgs e)
+        {
+            rtbTermo.Paste();
+        }
+
+        private void rtbTermo_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                toolStripFontFamily.Text = rtbTermo.SelectionFont.FontFamily.Name;
+                toolStripTamanho.Text = rtbTermo.SelectionFont.Size.ToString();
+            }
+            catch (Exception erro)
+            {
+                toolStripFontFamily.Text = String.Empty;
+                toolStripTamanho.Text = String.Empty;
+            }
+        }
+
+        private void toolStripFontFamily_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(toolStripFontFamily.Text))
+                return;
+
+            if (toolStripFontFamily.SelectedText == rtbTermo.SelectionFont.Name)
+                return;
+
+            rtbTermo.SelectionFont = new Font(toolStripFontFamily.Text, rtbTermo.SelectionFont.Size, rtbTermo.SelectionFont.Style);
+        }
+
+        private void toolStripTamanho_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(toolStripTamanho.Text))
+                return;
+
+            if (float.Parse(toolStripTamanho.Text) == rtbTermo.SelectionFont.Size)
+                return;
+
+            rtbTermo.SelectionFont = new Font(rtbTermo.SelectionFont.Name, float.Parse(toolStripTamanho.Text), rtbTermo.SelectionFont.Style);
+        }
+
+        private void toolStripNegrito_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionFont = new Font(rtbTermo.SelectionFont, rtbTermo.SelectionFont.Style ^ FontStyle.Bold);
+        }
+
+        private void toolStripItalico_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionFont = new Font(rtbTermo.SelectionFont, rtbTermo.SelectionFont.Style ^ FontStyle.Italic);
+        }
+
+        private void toolStripSublinhado_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionFont = new Font(rtbTermo.SelectionFont, rtbTermo.SelectionFont.Style ^ FontStyle.Underline);
+        }
+
+        private void toolStripRiscado_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionFont = new Font(rtbTermo.SelectionFont, rtbTermo.SelectionFont.Style ^ FontStyle.Strikeout);
+        }
+
+        private void toolStripCorFonte_Click(object sender, EventArgs e)
+        {
+            cdSelecionarCor.Color = rtbTermo.SelectionColor;
+
+            if (cdSelecionarCor.ShowDialog() == DialogResult.OK)
+                rtbTermo.SelectionColor = cdSelecionarCor.Color;
+        }
+
+        private void toolStripCorFundo_Click(object sender, EventArgs e)
+        {
+            cdSelecionarCor.Color = rtbTermo.SelectionBackColor;
+
+            if (cdSelecionarCor.ShowDialog() == DialogResult.OK)
+                rtbTermo.SelectionBackColor = cdSelecionarCor.Color;
+        }
+
+        private void toolStripEsquerda_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void toolStripCentralizado_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void toolStripDireita_Click(object sender, EventArgs e)
+        {
+            rtbTermo.SelectionAlignment = HorizontalAlignment.Right;
         }
     }
 }
